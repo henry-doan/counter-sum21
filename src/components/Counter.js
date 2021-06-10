544a7fc4ec77f88903c05f20b0a01dd3f7e37701
Henry Doan <henrydoan96@gmail.com>
2021-06-09 19:31:01 -0600

    end of counter

---
+++ src/components/Counter.js
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+
+class Counter extends Component {
+  state = { counterVal: 0, age: 23, email: "adfasdf" }
+
+  inc = () => {
+    this.setState({ counterVal: this.state.counterVal + 1})
+  }
+
+  dec = () => {
+    this.setState({ counterVal: this.state.counterVal - 1})
+  }
+  render() {
+    const { counterVal, age, email } = this.state 
+    const { color } = this.props
+    return (
+      <>
+        <h1>{color} Counter</h1>
+        <p style={{background: color}}>
+          {/* this is where my counter is */}
+          {counterVal}
+          <button onClick={this.inc}>+</button>
+          <button onClick={this.dec}>-</button>
+        </p>
+      </>
+    )
+  }
+}
+
+export default Counter;
